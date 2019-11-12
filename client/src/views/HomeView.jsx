dd31b56a5cf9e5b2098672a6e3b7cc26d52e13d8
Isabel Cruz <isabel.cruz81699@gmail.com>
2019-11-11 20:54:02 -0600

    sidebar front-end part 1

--- client/src/views/HomeView.jsx
+++ client/src/views/HomeView.jsx
@@ -3,33 +3,48 @@ import {ButtonGroup, Button, ListGroup, Image} from 'react-bootstrap';
 import {useState} from 'react';
 import {Link} from 'react-router-dom';
 import {MdEmail, MdLock, MdPerson} from 'react-icons/md';
-import {FaGraduationCap, FaSortNumericUp} from 'react-icons/fa';
-import {IoIosRocket} from 'react-icons/io';
+import {FaHome, FaSearch, FaInbox, FaChalkboardTeacher} from 'react-icons/fa';
+import {IoIosRocket, IoIosNotifications} from 'react-icons/io';
+import {FiPower} from 'react-icons/fi';
 
 function HomeView() {
   return (
     // sidebar
     <div style={styles.container}>
-      <div style={styles.profile}>
-        <Image
-          style={styles.profileImg}
-          src={require('../img/userprofile.png')}
-        />
-      </div>
-      <ListGroup style={styles.options} as="ul" variant="flush">
-        <ListGroup.Item action active>
+      <Image
+        style={styles.profileImg}
+        src={require('../img/userprofile.png')}
+      />
+      <ListGroup as="ul" variant="flush">
+        <ListGroup.Item style={styles.listItem} action active>
+          <FaHome style={styles.icon}></FaHome>
           Home
         </ListGroup.Item>
-        <ListGroup.Item action>Search</ListGroup.Item>
-        <ListGroup.Item action>Notifications</ListGroup.Item>
-        <ListGroup.Item action>Inbox</ListGroup.Item>
-        <ListGroup.Item action>My tutors</ListGroup.Item>
+        <ListGroup.Item style={styles.listItem} action>
+          <FaSearch style={styles.icon}></FaSearch>
+          Search
+        </ListGroup.Item>
+        <ListGroup.Item style={styles.listItem} action>
+          <IoIosNotifications style={styles.icon}></IoIosNotifications>
+          Notifications
+        </ListGroup.Item>
+        <ListGroup.Item style={styles.listItem} action>
+          <FaInbox style={styles.icon}></FaInbox>
+          Inbox
+        </ListGroup.Item>
+        <ListGroup.Item style={styles.listItem} action>
+          <FaChalkboardTeacher style={styles.icon}></FaChalkboardTeacher>
+          My tutors
+        </ListGroup.Item>
         <ListGroup.Item style={styles.tutorBtnContainer}>
           <Button variant="primary" block>
             Become tutor
           </Button>
         </ListGroup.Item>
-        <ListGroup.Item action>Log out</ListGroup.Item>
+        <ListGroup.Item style={styles.listItem} action>
+          <FiPower style={styles.icon}></FiPower>
+          Log out
+        </ListGroup.Item>
       </ListGroup>
     </div>
   );
@@ -42,26 +57,18 @@ const styles = {
     width: '20%',
     height: '100%',
     minWidth: '300px',
-    border: 'solid black',
+    borderRight: '#bdbdbd solid 2px',
   },
-  profile: {
+  profileImg: {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
-    border: 'solid black',
     borderRadius: '50%',
-    width: '200px',
-    height: '200px',
-    alignSelf: 'center',
-    marginTop: '10px',
-  },
-  profileImg: {
-    width: '150px',
-    height: '150px',
-  },
-  options: {
-    padding: '5px',
+    width: '70px',
+    height: '70px',
+    alignSelf: 'flex-start',
+    marginLeft: '10px',
     marginTop: '10px',
     marginBottom: '10px',
   },
@@ -69,6 +76,13 @@ const styles = {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  listItem: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  icon: {
+    margin: '8px',
   },
 };
 
