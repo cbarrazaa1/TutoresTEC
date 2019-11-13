15a3b44fe2a67da6e3c4e756326f247870df2c61
Isabel Cruz <isabel.cruz81699@gmail.com>
2019-11-13 12:13:36 -0600

    posts front end

--- client/src/views/HomeView.jsx
+++ client/src/views/HomeView.jsx
@@ -1,14 +1,21 @@
 import * as React from 'react';
 import {useState} from 'react';
+import {Card} from 'react-bootstrap';
 import {Link, Switch, Route} from 'react-router-dom';
 import SideBar from '../components/SideBar';
+import Post from '../components/Post';
 
 function HomeView() {
   return (
     <div style={styles.root}>
       <SideBar />
       <Switch>
-        <Route path="/home"></Route>
+        <Route path="/home">
+          <div style={styles.container}>
+            <h3 style={styles.title}>Latest Posts</h3>
+            <Post title="This is a post" author="tutor" description="aodfiajsdiofaenfnaeoif aifjadnf aeifae i" />
+          </div>
+        </Route>
       </Switch>
     </div>
   );
@@ -20,6 +27,17 @@ const styles = {
     flexDirection: 'row',
     width: '100%',
     height: '100%',
+    padding: '10px',
+  },
+  title: {
+    fontWeight: 200,
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+    height: '100%',
+    padding: '10px',
   },
 };
 
