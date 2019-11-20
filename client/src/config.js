16649c209cb5570742c21fdc32f05035b9a0e525
Cesar Barraza <cbarrazaa1@gmail.com>
2019-11-20 01:32:00 -0600

    fix server url

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-export const SERVER_URL = 'http://tutores-tec-itesm.herokuapp.com';
+export const SERVER_URL = 'https://tutores-tec-itesm.herokuapp.com';
 //export const SERVER_URL = 'http://localhost:3001';
